1f0f1e8d90fdae8fa46fbd5b1b47dfee917c90b6
«musik <hjccbz1hjccbz@gmail.com>
2022-08-05 21:40:18 +0300

    пытаюсь победить цикл, но цикл побеждает меня...

--- Lesson_01_homework_04/Homework_04.cs
+++ Lesson_01_homework_04/Homework_04.cs
@@ -1,12 +1,14 @@
 ﻿//Задание 4
 
-Console.WriteLine("Введите число: ");
-int numberX = Convert.ToInt32(Console.ReadLine());
-int number = 0;
-for (int number++)
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+int numberX = 1;
+while (numberX > number)
 {
-if (number%==0)
-{
-    Console.WriteLine(number)
-}
+    if (numberX % 2 == 0)
+    {
+       Console.WriteLine( numberX ); 
+       
+       numberX++;
+    }        
 }
