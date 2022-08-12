71f73a1bc9b112c101194144189c108259fb97fa
«musik <hjccbz1hjccbz@gmail.com>
2022-08-12 19:16:15 +0300

    не доделал 2 дз 2 задание

---
+++ Lesson_02_homework_01/Lesson_02_homework_01.cs
@@ -0,0 +1,6 @@
+﻿//Задача №1
+int mainNumber = new Random().Next(100, 1000);
+int number = mainNumber / 10;
+int second = number % 10;
+Console.WriteLine($"Вторая цифра из числа {mainNumber} равна: {second} " );
+// Уверен можно сделать по другому, ещё меньне, на строку
