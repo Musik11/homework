d5fa5258cc5c134754d57e771b640841aae6efcf
«musik <hjccbz1hjccbz@gmail.com>
2022-08-16 22:56:13 +0300

    начал делать дз

--- Lesson_02/Lesson_02_homework_02/Lesson_02_homework_02.cs
+++ Lesson_02/Lesson_02_homework_02/Lesson_02_homework_02.cs
@@ -1,4 +1,4 @@
 ﻿//Задача №2
 int mainNumber = new Random().Next(10, 100000);
 Console.WriteLine(mainNumber);
-Console.WriteLine(mainNumber>99 ? mainNumber.ToString()[2] : '-')
+Console.WriteLine(mainNumber>99 ? mainNumber.ToString()[2] : '-');
