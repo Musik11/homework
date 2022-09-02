a869f302952a74f4656e6b8ec2a61ce337fff06a
«musik <hjccbz1hjccbz@gmail.com>
2022-09-02 09:20:41 +0300

    мозг кипит уже

--- Lesson_06/Lesson_06_homework_01/Lesson_06_homework_01.cs
+++ Lesson_06/Lesson_06_homework_01/Lesson_06_homework_01.cs
@@ -3,7 +3,7 @@
 Console.Write("Insert your numbers (use space, pls): "); 
 int[] user_arr = Array.ConvertAll(Console.ReadLine().Split(),int.Parse);  
 
-int count = 0;                                            // запуск счетчика
+int count = 0;                                           
 
 for (int i = 0; i < user_arr.Length; i++)
 {
